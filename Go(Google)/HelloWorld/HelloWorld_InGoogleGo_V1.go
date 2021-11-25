0d19ef5f38655d0f2e2f8bd63853addd78bf9380
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-25 14:25:12 -0800

    Added the Go (Google) support directory (6 files)
    Added the Google Go support directory, containing 6 files in Go2009 format.

---
+++ Go(Google)/HelloWorld/HelloWorld_InGoogleGo_V1.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" // Import Formatting settings
+
+func main()
+{ // This is the way I associate curly bracket usage in Go, as I see examples written like this, with the curly bracket wasting an extra line
+    fmt.Println("Hello, world!")
+}
